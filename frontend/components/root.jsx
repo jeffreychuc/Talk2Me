dfb8d6f4c7431e1c081611b91ce7217e0c229ad8
Jeffrey Chuc <jeffreychuc@gmail.com>
2018-02-16 20:55:34 -0500

    add ability to add more than 2 chat windows in one browser

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -6,14 +6,39 @@ import Draggable from 'react-draggable';
 import ChatWindow from './chatwindow';
 
 // needed for two distinct socket instances
-const socket0 = io();
-const socket1 = io();
+const socketArray = [];
 
 class Root extends React.Component {
+  constructor(props) {
+    super(props);
+    let socketArray = [];
+    for (let i = 0; i < 2; i++) {
+      socketArray.push(io());
+    }
+    console.log(socketArray);
+    this.state={ socketArray };
+    this.renderChatInstance = this.renderChatInstance.bind(this);
+  }
+
+  renderChatInstance(socket) {
+    return (
+      <Draggable handle="strong">
+        <div className="box no-cursor">
+          <strong className="cursor">
+            <div className='topBar' />
+          </strong>
+          <ChatWindow socket={socket} />
+        </div>
+      </Draggable>
+    );
+  }
+  
   render() {
+    let { socketArray } = this.state;
     return (
       <div className='mainChats'>
-        <Draggable handle="strong">
+        {socketArray.map((socket) => this.renderChatInstance(socket))}
+        {/* <Draggable handle="strong">
           <div className="box no-cursor">
             <strong className="cursor">
               <div className='topBar' />
@@ -31,7 +56,8 @@ class Root extends React.Component {
             </strong>
             <ChatWindow socket={socket1} />
           </div>
-        </Draggable>
+        </Draggable> */}
+
       </div>
     );
   }
